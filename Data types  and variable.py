dae278b8cbacb5cd0f4e85e20a6818b2a76ac247
[Binod Ojha] <[ojhabinod777.]>
2026-01-18 15:48:48 +0545

    .

--- Data types  and variable.py
+++ Data types  and variable.py
@@ -1,1 +1,19 @@
-a
+# intermediate level code
+a = input("Enter anything: ")
+
+try:
+    a = int(a)
+except ValueError:
+    try:
+        a = float(a)
+    except ValueError:
+        pass
+
+print(type(a))
+# to get there first basic to know
+
+a=123 # it is a integer
+b=1.2 #it is a float
+c=(5>3) # it is a booleans
+
+
